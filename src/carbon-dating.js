121eb4a45548520264c43a593d78ef8682645315
Enya <enya1407@gmail.com>
2020-02-24 14:53:17 +0300

    feat: ready 7 of 10

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,17 @@ const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== "string") return false;
-  0 <= N < 15;
+  if (typeof sampleActivity !== "string" || isNaN(parseFloat(sampleActivity)))
+    return false;
+
+  let result = Math.round(
+    Math.ceil(
+      Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) /
+      (0.693 / HALF_LIFE_PERIOD),
+    ),
+  );
+
+  if (result < 1 || !isFinite(result)) return false;
+
+  return result;
 };
